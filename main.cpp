542699cc94bf3482eb5117bc4aad7a34e3227991
Badnuker <113335677+Badnuker@users.noreply.github.com>
2023-08-17 14:37:59 +0800

    feat: 新增抽取中特效

--- main.cpp
+++ main.cpp
@@ -3,22 +3,59 @@
 #include<random>
 #include<chrono>
 #include"windows.h"
+#define white 0
+#define green 2
 using namespace std;
 
-string name;
+string name, final;
 
 vector<string> ans;
 
-int main() {
-	mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
-	cout<<"Enter names separated by spaces or line breaks, ending with \"end\" :\n"; 
-	while(cin>>name){
-		if(name=="end") break;
+void Color(int color) {
+	switch (color) {
+		case 0:
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+			break;
+		case 2:
+			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_GREEN);
+			break;
+	}
+}
+
+void Input() {
+	Color(white);
+	cout << "Enter names separated by spaces or line breaks, ending with \"end\" :\n";
+	while (cin >> name) {
+		if (name == "end") {
+			break;
+		}
 		ans.push_back(name);
 	}
-	cout << '\n';
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_GREEN);
-	cout << ans[rnd() % ans.size()] << '\n';
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
+}
+
+void Extract() {
+	mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+	cout << "\nExtracting...\n\n";
+	Color(green);
+	for (int i = 1; i <= 48; i++) {
+		cout << ans[rnd() % ans.size()] << '\r';
+		Sleep(100);
+	}
+	Color(white);
+	final = ans[rnd() % ans.size()];
+}
+
+void Output() {
+	cout << "----------\n";
+	Color(green);
+	cout << final << '\n';
+	Color(white);
+	cout << "----------\n";
+}
+
+int main() {
+	Input();
+	Extract();
+	Output();
 	return 0;
 }
