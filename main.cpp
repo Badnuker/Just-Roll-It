f00e635baa5a059b12cca7ab09844de1379c1c0a
Badnuker <badnuker@126.com>
2023-09-02 19:37:25 +0800

    refactor: 重构了部分代码

--- main.cpp
+++ main.cpp
@@ -1,48 +1,21 @@
-#include<iostream>
-#include<vector>
-#include<random>
-#include<chrono>
+#include<bits/stdc++.h>
+
 #include"windows.h"
+#include"my_console.h"
+
 #define white 0
 #define green 2
+
 using namespace std;
 
-string input_name, final_name, empty_string;
-
 int maxLen;
+string input_name, final_name, empty_string, horizontal_rule;
 
 vector<string> input_names;
 
-void Color(int color) {
-	switch (color) {
-		case 0:
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_RED | FOREGROUND_GREEN | FOREGROUND_BLUE);
-			break;
-		case 2:
-			SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), FOREGROUND_INTENSITY | FOREGROUND_GREEN);
-			break;
-	}
-}
-
-void HideCursor() {
-	CONSOLE_CURSOR_INFO cursor_info = {1, 0};
-	SetConsoleCursorInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);
-}
-
-pair<short, short> GetCursorPosition() {
-	CONSOLE_SCREEN_BUFFER_INFO cursor_info;
-	GetConsoleScreenBufferInfo(GetStdHandle(STD_OUTPUT_HANDLE), &cursor_info);
-	return {cursor_info.dwCursorPosition.X, cursor_info.dwCursorPosition.Y};
-}
-
-void SetCursorPosition(short x, short y) {
-	COORD coord = {x, y};
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), coord);
-}
-
 void LoadingScreen() {
 	SetConsoleTitleA("Loading...");
-	Color(white);
+	SetConsoleColor(white);
 	SetConsoleTitleA("Just-Row-it! By Badnuker");
 }
 
@@ -62,30 +35,33 @@ void Input() {
 
 void Extract() {
 	mt19937 rnd(chrono::steady_clock::now().time_since_epoch().count());
+	HideCursor();
 	cout << "\nExtracting...\n\n\n";
-	Color(green);
+	SetConsoleColor(green);
 	for (int i = 1; i <= 50; i++) {
 		cout << input_names[rnd() % input_names.size()] << '\r';
 		Sleep(150);
 		cout << empty_string << '\r';
 	}
-	Color(white);
+	SetConsoleColor(white);
 	final_name = input_names[rnd() % input_names.size()];
+	for (int i = 0; i < (int)final_name.size(); i++) {
+		horizontal_rule = '-' + horizontal_rule;
+	}
 }
 
 void Output() {
 	SetCursorPosition(GetCursorPosition().first, GetCursorPosition().second - 1);
-	cout << "----------\n";
-	Color(green);
+	cout << horizontal_rule << '\n';
+	SetConsoleColor(green);
 	cout << final_name << '\n';
-	Color(white);
-	cout << "----------\n";
+	SetConsoleColor(white);
+	cout << horizontal_rule << '\n';
 }
 
 int main() {
 	LoadingScreen();
 	Input();
-	HideCursor();
 	Extract();
 	Output();
 	system("pause");
